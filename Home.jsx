80280817cdefaed0f83e9bae23df91f1fc1a460e
1804thao <1804thao@gmail.com>
2025-11-15 22:06:02 +0700

    thêm code tới map

---
+++ Home.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <div
+      style={{
+        height: "100vh",
+        width: "100vw",
+        backgroundImage: `url("/background_0.png")`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        color: "white",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        textAlign: "center",
+      }}
+    >
+      <Navbar />
+      <main style={{ marginTop: "100px" }}>
+        {/* Nội dung chính ở đây nếu cần */}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
